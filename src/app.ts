1d356d76e16d4a755120fa83695b97b287e99a90
fadeev <fadeev@strelkainstitute.com>
2020-08-22 21:31:56 +0300

    Configure CORS: allow all origins

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import Fastify, {FastifyInstance} from 'fastify';
 import api from './api';
+import fastifyCors from 'fastify-cors';
 
 export default function App(): FastifyInstance {
     const app = Fastify({ logger: true });
+
+    app.register(fastifyCors, {origin: '*'});
 
     app.get('/', async (_request, _reply) => 'Hello world!');
     app.register(api, {prefix: '/api'});
