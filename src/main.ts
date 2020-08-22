6d93ad575eb50d4da0253770e4f9e561af8d3a2f
fadeev <fadeev@strelkainstitute.com>
2020-08-22 19:38:03 +0300

    Add fastify

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-console.log('Hello world!');
+import Fastify from 'fastify';
+
+async function main() {
+    const app = Fastify({ logger: true });
+
+    app.get('/', async (_request, _reply) => 'Hello world!');
+
+    try {
+        await app.listen(3000);
+    } catch (e: unknown) {
+        console.error(e);
+        process.exit(1);
+    }
+}
+
+main();
